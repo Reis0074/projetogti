1d90153d279372d1d5c90b73efd58f4e991fdc0c
Reis0074 <abinerreis@outlook.com>
2025-03-20 10:57:15 -0300

    estudo sobre rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste', function () {
+    return "O codigo foi testado";
+});
+
+Route::get('/usuario/{nome}', function ($nome) {
+    return "O usuário atual é: ".$nome;
+});
+
+Route::get('/sum/{num1}/{num}', function ($num1,$num2) {
+    return "A soma é: ".$num1 + $num2;
+});
+
+Route::get('/subtraction/{num1}/{num}', function ($num1,$num2) {
+    return "A soma é: ".$num1 - $num2;
+});
+
+Route::get('/division/{num1}/{num}', function ($num1,$num2) {
+    return "A soma é: ".$num1 / $num2;
+});
+
+Route::get('/multiplication/{num1}/{num}', function ($num1,$num2) {
+    return "A soma é: ".$num1 * $num2;
+});

3b184332b375687e0922eb3901aae725ede35d13
Reis0074 <abinerreis@outlook.com>
2025-06-26 09:07:17 -0300

    boxes

--- resources/views/cadastra-aula.blade.php
+++ resources/views/cadastra-aula.blade.php
@@ -20,11 +20,26 @@
               </div>
               <div class="sm:col-span-2">
                   <label for="semestre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Semestre</label>
-                  <textarea id="semester" name="semestre" rows="1" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Digite o semestre"></textarea>
-              </div>        
+                  <select name="semestre" id="semestre" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                  </select>
+                </div>        
               <div class="sm:col-span-2">
                   <label for="curso" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Curso</label>
-                  <textarea id="curso" name="curso" rows="1" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Digite o curso"></textarea>
+                  <select name="curso" id="curso" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" >
+                    <option value="Análise e Desenvolvimento de Sistemas (AMS)">Análise e Desenvolvimento de Sistemas (AMS)</option>
+                    <option value="Automação Industrial">Automação Industrial</option>
+                    <option value="Gestão de Tecnologia da Informação (GTI)">Gestão de Tecnlogia da Informação (GTI)</option>
+                    <option value="Gestão Empresarial">Gestão Empresarial</option>
+                    <option value="Manutenção Industrial">Manutenção Industrial</option>
+                    <option value="Produção Fonográfica">Produção Fonográfica</option>
+                    <option value="Processos Gerencias (AMS)">Processos Gerencias (AMS)</option>
+                  </select>
               </div>
           </div>
           <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-green-600">
--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -1,27 +1,9 @@
 @vite(['resources/css/app.css','resources/js/app.js'])
 
-
+<x-topo></x-topo>
 
 <header>
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://www.fatectatui.edu.br/site4/index.php" target="_blank" class="flex items-center">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf6Tu5Uk894gPZ-1DFm4Psa4trF4ZmYLfLPg&s" class="mr-3 w-26 " alt="Flowbite Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Curso GTI</span>
-            </a>
 
-            <div class="flex items-center lg:order-2">
-                @guest
-
-                <a href="login" class="text-gray-800 dark:text-white hover:bg-blue-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Logar</a>
-                <a href="criar-conta" class="text-white hover:bg-blue-400 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Criar Conta</a>
-
-                @else
-                <div class=" text-gray-800 dark:text-white font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 ">Ola {{Auth::user()->name}}
-
-                </div>
-                <a href="logout" class="text-white hover:bg-blue-400 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-red-500 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-blue-800">Sair</a>
-                @endguest
                 <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -30,32 +12,7 @@
 
         </div>
 
-            <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                   <li>
-                        <a href="/" class="block py-2 pr-4 pl-3 text-gray-800 rounded bg-blue-700 lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white" aria-current="page">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="sobre" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Sobre</a>
-                    </li>
-                    <li>
-                        <a href="aula" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Aulas</a>
-                    </li>
-                    <li>
-                        <a href="recursos" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Recursos</a>
-                    </li>
-                    <li>
-                        <a href="lista-equipe" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Equipe</a>
-                    </li>
-                    <li>
-                        <a href="fale-conosco" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Fale Conosco</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <nav class="bg-white border-gray-200 px- lg:px-6 py-2.5  dark:bg-gray-600">
+    <nav class="bg-gray h-screen border-gray-200 px- lg:px-6 py-2.5  dark:bg-gray-600">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="https://www.fatectatui.edu.br/site4/cursos.php" target="_blank" class="flex items-center">
                 <img src="https://www.fatectatui.edu.br/site4/imagens/icone7SA.png" class="mr-3 w-29 " alt="Flowbite Logo" />
